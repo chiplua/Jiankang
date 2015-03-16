f3fcb5fc1b1eaaa8a8c68e22ea99b7679105f737
chiplua <chiplua@gmail.com>
2015-03-16 17:26:08 +0800

    [TITLEBAR]:Add titlebar

--- src/main/java/com/chiplua/jiankang/JiankangActivity.java
+++ src/main/java/com/chiplua/jiankang/JiankangActivity.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class JiankangActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_jiankang);
+        this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
     }
 
 
