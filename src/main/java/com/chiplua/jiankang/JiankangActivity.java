3dfdb188c130d7a3efe3ea6fda8d5fab7f9ad6b9
chiplua <chiplua@gmail.com>
2015-03-17 16:46:11 +0800

    [LISTVIEWADAPTER]:Add listviewadapter

--- src/main/java/com/chiplua/jiankang/JiankangActivity.java
+++ src/main/java/com/chiplua/jiankang/JiankangActivity.java
@@ -5,17 +5,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class JiankangActivity extends Activity {
+    private ListView libraryList = null;
+    private LibraryListViewAdapter listViewAdapter = null;
+    private List<Map<String, Object>> listItems;
+    private String[] libraryName = {"化验单助手", "常见病介绍", "育儿介绍"};
+    private Integer[] imgIDs = {R.drawable.icon_test, R.drawable.icon_common_ill,
+                                R.drawable.icon_child};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_jiankang);
         this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+        libraryList = (ListView) findViewById(R.id.library_list);
+        listItems = getListItems();
+        listViewAdapter = new LibraryListViewAdapter(this, listItems);
+        libraryList.setAdapter(listViewAdapter);
     }
 
+    private List<Map<String, Object>> getListItems(){
+        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < libraryName.length; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("image", imgIDs[i]);
+            map.put("text", libraryName[i]);
+            map.put("into", R.drawable.right_arrow);
+            listItems.add(map);
+        }
+        return listItems;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ src/main/java/com/chiplua/jiankang/LibraryListViewAdapter.java
@@ -0,0 +1,79 @@
+package com.chiplua.jiankang;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by chiplua_client on 15-3-17.
+ */
+public class LibraryListViewAdapter extends BaseAdapter {
+    private static final String TAG = "LibraryListViewAdapter";
+    private static Context mContext = null;
+    private List<Map<String, Object>> mListItems;
+    private LayoutInflater listContainer;
+
+    public final class ListItemView {
+        public ImageView image;
+        public TextView text;
+        public ImageView into;
+    }
+
+    public LibraryListViewAdapter(Context context, List<Map<String, Object>> listItems) {
+        mContext = context;
+        mListItems = listItems;
+        listContainer= LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        return mListItems.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListItemView  listItemView = null;
+        if (convertView == null) {
+            listItemView = new ListItemView();
+            convertView = listContainer.inflate(R.layout.list_item, null);
+
+            listItemView.image = (ImageView) convertView.findViewById(R.id.image_item);
+            listItemView.text = (TextView) convertView.findViewById(R.id.library_name);
+            listItemView.into = (ImageView) convertView.findViewById(R.id.into_item);
+
+            convertView.setTag(listItemView);
+        } else {
+            listItemView = (ListItemView)convertView.getTag();
+        }
+
+        listItemView.image.setBackgroundResource((Integer) mListItems.get(position).get("image"));
+        listItemView.text.setText((String) mListItems.get(position).get("text"));
+        listItemView.into.setBackgroundResource((Integer) mListItems.get(position).get("into"));
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick");
+            }
+        });
+        return convertView;
+    }
+}
