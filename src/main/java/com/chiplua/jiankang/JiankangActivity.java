563f256f6c50e52c2d69c1b6c7c78a3696c9e1cf
chiplua <chiplua@gmail.com>
2015-03-18 15:11:43 +0800

    [FAVORITE_FILE]:Add favorite file activity

--- src/main/java/com/chiplua/jiankang/JiankangActivity.java
+++ src/main/java/com/chiplua/jiankang/JiankangActivity.java
@@ -2,10 +2,14 @@ package com.chiplua.jiankang;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,11 +19,14 @@ import java.util.Map;
 
 public class JiankangActivity extends Activity {
     private ListView libraryList = null;
+    private static ImageButton backButton = null;
+    private static TextView titleName = null;
+    private LayoutInflater inflater = null;
     private LibraryListViewAdapter listViewAdapter = null;
     private List<Map<String, Object>> listItems;
-    private String[] libraryName = {"化验单助手", "常见病介绍", "育儿介绍"};
+    private String[] libraryName = {"化验单助手", "常见病介绍", "育儿介绍", "收藏夹"};
     private Integer[] imgIDs = {R.drawable.icon_test, R.drawable.icon_common_ill,
-                                R.drawable.icon_child};
+                                R.drawable.icon_child, R.drawable.icon_favorite};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,18 @@ public class JiankangActivity extends Activity {
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_jiankang);
         this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+
         libraryList = (ListView) findViewById(R.id.library_list);
         listItems = getListItems();
         listViewAdapter = new LibraryListViewAdapter(this, listItems);
         libraryList.setAdapter(listViewAdapter);
+
+        backButton = (ImageButton) findViewById(R.id.back);
+        backButton.setVisibility(View.GONE);
+        titleName = (TextView) findViewById(R.id.title_name);
+        titleName.setText(R.string.healty_library);
     }
+
 
     private List<Map<String, Object>> getListItems(){
         List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
